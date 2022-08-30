ef27a0372b3a48b7fc0b84b796fedc7218720398
ahmad almuhidat <ahmad.almuhidat@gmail.com>
2022-08-30 22:03:30 +0300

    fixing headers

--- models/Members.js
+++ models/Members.js
@@ -11,6 +11,7 @@ module.exports = {
       await pool.query(sqlQuery, [OperationID], (err, results) => {
         if (err) console.log(err);
         if (results) {
+          res.header("Access-Control-Allow-Origin", "*");
           res.status(200).json({ data: results });
         }
       });
