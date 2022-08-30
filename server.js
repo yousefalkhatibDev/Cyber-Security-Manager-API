0381dd8872f5b8686a9684741ca01e909f28068d
ahmad almuhidat <ahmad.almuhidat@gmail.com>
2022-08-30 21:51:35 +0300

    fixing cors

--- server.js
+++ server.js
@@ -10,8 +10,23 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
 // Middleware
 app.use(express.json());
+app.use(allowCrossDomain)
 app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
